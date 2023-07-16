0367f010aa189f7a07558ecd2bd9bd5199a26146
Carlos Gitto <carlosgitto98@gmail.com>
2023-07-16 11:39:03 -0300

    Prevent swap between same index

--- sorters/base.py
+++ sorters/base.py
@@ -37,6 +37,8 @@ def profiler(sorter_func):
 
 
 def swap_elements(l1: list, i1: int, i2: int):
+    if i1 == i2:
+        raise IndexError(f"Indexes to swap should not be equal (i1:{i1} i2:{i2})")
     l1[i1], l1[i2] = l1[i2], l1[i1]
     return l1
 
