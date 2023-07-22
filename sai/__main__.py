b958c8515c3eaeae59dbeaa7e6277084a7c81ab3
Carlos Gitto <carlosgitto98@gmail.com>
2023-07-22 12:09:53 -0300

    Update sai to use async/await. Now results are written to a parquet file

--- sai/__main__.py
+++ sai/__main__.py
@@ -1,46 +1,94 @@
+from sai.utils import dataset_generator, SorterFactory, Sorter, save_df_to_parquet
+from sai.globals import RANGES, SIZES, RESULT_COLUMNS, DATASET_COLUMNS, REPEAT_NUM
+from sai.logs_handler import Logger
+import asyncio
+import pandas as pd
+import uuid
 import sys
-from sai.utils import generate_dataset, SorterFactory
 import time
-import numpy as np
-import glob
 
-args = sys.argv
+st = time.time()
+
+
+def sort_data(sorter: Sorter, data: list):
+    return sorter.sort_data(data.copy())
+
+
+async def create_coroutine(func, *args, **kwargs):
+    return func(*args, **kwargs)
+
+
+async def run_task_group(cors):
+    async with asyncio.TaskGroup() as tg:
+        tasks = [tg.create_task(cor) for cor in cors]
+    return [t.result() for t in tasks]
+
+
+# PATHs
+UNIQUE_ID = uuid.uuid4()
+OUT_PATH = f"./analysis/results/{UNIQUE_ID}.parquet"
+LOG_PATH = f"./analysis/logs/{UNIQUE_ID}.log"
+logg = Logger(LOG_PATH).logger
+
+# Create sorter based on sorter type
+sorters = SorterFactory().create_all_sorters()
+
+# Init result dataframe
+result_df = pd.DataFrame(columns=RESULT_COLUMNS)
+config_data = []
 
 try:
-    min_value = int(args[1])
-    max_value = int(args[2])
-    size = int(args[3])
-    strat = args[4]
+    for (dataset, sorted_dataset), *dataset_config in dataset_generator(
+        ranges=RANGES, sizes=SIZES, repeat=REPEAT_NUM
+    ):
+        corr = (
+            # Coroutines for dataset
+            [
+                create_coroutine(sort_data, sorter=sorter, data=dataset)
+                for sorter in sorters
+            ]
+            +
+            # Coroutines for sorted dataset
+            [
+                create_coroutine(sort_data, sorter=sorter, data=sorted_dataset)
+                for sorter in sorters
+            ]
+        )
+
+        # Run group of coroutines
+        results = asyncio.run(run_task_group(corr))
+
+        # Concat results in a result_df
+        result_df = pd.concat(
+            [result_df, pd.DataFrame(results, columns=RESULT_COLUMNS)],
+            ignore_index=True,
+        )
+
+        # Save config used
+        config_data.extend((dataset_config,) * len(corr))
+except KeyboardInterrupt:
+    logg.warning(
+        f"Cancel before all experiments were completed.",
+    )
+    logg.info(
+        f"Saving results on {OUT_PATH}.",
+    )
+except Exception as e:
+    logg.error("HEllo")
+    logg.error(str(e))
 except:
-    print(f"Correct usage of SAI is: python -m sai MIN MAX DATASET_SIZE SORTER_NAME")
-    sys.exit(1)
+    logg.error("NO HEllo")
 
-# Generate Dataset
-dataset = generate_dataset(min_value, max_value, size)
-# Create sorter based on sorter type
-sorter = SorterFactory().create_sorter(strat)
 
-# Sort generated data
-result = sorter.sort_data(dataset)
+# Set config df
+config_df = pd.DataFrame(config_data, columns=DATASET_COLUMNS)
 
-# Write results to corresponding file
-path = f"./analysis/results/{str(sorter)}.csv"
-paths = glob.glob(path)
-if len(paths) > 0:
-    with open(path, "a") as f:
-        f.write(
-            f"{size},{min_value},{max_value},"
-            + ",".join(str(item) for item in result)
-            + "\n"
-        )
+# Add configs to result_df
+df = pd.concat([result_df, config_df], axis=1)
 
-else:
-    with open(path, "w+") as f:
-        f.write(
-            "size,min_value,max_value,elapsed_time,memory_size,memory_peak,success,inp_sorted,out_sorted,success,error\n"
-        )
-        f.write(
-            f"{size},{min_value},{max_value},"
-            + ",".join(str(item) for item in result)
-            + "\n"
-        )
+
+save_df_to_parquet(df, OUT_PATH)
+
+logg.info(f"Elapsed time {time.time()-st} seconds.")
+logg.info(f"A complete log of this run can be found in {OUT_PATH}")
+sys.exit(0)
